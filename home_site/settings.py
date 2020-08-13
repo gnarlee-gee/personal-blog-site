3432910b23c018a61d9221d3dfd4f8afe1e4b91e
gnarlee-gee <charlesgrealy@gmail.com>
2020-08-13 18:13:55 -0500

    changed allowed host

--- home_site/settings.py
+++ home_site/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') # Instead of your actual secret key
 
 DEBUG = os.environ.get('DEBUG_VALUE', '') == 'True'
 
-ALLOWED_HOSTS = ['.herokuapp.com/']
+ALLOWED_HOSTS = ['charles-grealy.herokuapp.com/']
 
 
 # Application definition
