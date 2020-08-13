b80916bd464edebf333d827982fb5f73926d600b
gnarlee-gee <charlesgrealy@gmail.com>
2020-08-13 18:06:21 -0500

    pushing to heroku

--- home_site/settings.py
+++ home_site/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY') # Instead of your actual secret key
 
 DEBUG = os.environ.get('DEBUG_VALUE', '') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com/']
 
 
 # Application definition
