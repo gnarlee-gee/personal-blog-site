5813c8e1639a64b19ce1ca79c95eda772599b6f3
gnarlee-gee <charlesgrealy@gmail.com>
2020-08-15 20:01:30 -0500

    changed date/time for posts

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from pagedown.widgets import AdminPagedownWidget
-
+from datetime import datetime  
 # Note, if you add another field when you already have fields filled
 # make sure to run makemigrations and migrate!
 
+
+
+
+
+# print(timezone.now)
 
 # Create your models here.
 class Post(models.Model):
@@ -16,6 +21,11 @@ class Post(models.Model):
     # on_delete will delete all posts by admin IF we delete that admin
     author = models.ForeignKey(User, on_delete=models.CASCADE) 
     body = models.TextField()
+    
+    date = models.DateField(auto_now_add=False)
+    
+    class Meta:
+        ordering = ['-date']
 
     def __str__(self):
         '''
