010b837b394b9209af4036a87950cb3913ea037f
gnarlee-gee <charlesgrealy@gmail.com>
2020-09-20 21:54:07 -0500

    complete scrollbars and nav bar

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -1,18 +1,28 @@
 // Grab the info div and change it's z-index to allow scrolling above nav divs
-
-
-
-
-function setAtts() {
+function hideAndShow() {
     var checkedItem = document.getElementById('hamburger');
     var changeZindex = document.getElementById('info-box');
+    var hideName = document.getElementById('home-link');
+    var hideNavBar = document.getElementById('nav-bar');
+    var checkLabel = document.getElementById('check-label')
+    changeZindex.setAttribute('style', 'z-index: 1;');
     checkedItem.addEventListener('click', event => {
         if (checkedItem.checked) {
             changeZindex.setAttribute('style', 'z-index: 0;');
+            changeZindex.setAttribute('style', 'opacity: 0;');
+            hideName.setAttribute('style', 'opacity: 0;');
+            hideNavBar.setAttribute('style', 'background: rgba(32, 32, 32, 1)');
+            checkLabel.setAttribute('style', 'border: 4px solid transparent;');
         } else {
             changeZindex.setAttribute('style', 'z-index: 1;');
+            changeZindex.setAttribute('style', 'opacity: 1;');
+            hideName.setAttribute('style', 'opacity: 1;');
+            hideNavBar.setAttribute('style', 'opacity: 1;');
+            hideNavBar.removeAttribute('style', 'background: rgba(32, 32, 32, 1)');
+            checkLabel.removeAttribute('style', 'border: 4px solid transparent;');
         }
     })
 }
 
-window.onload = setAtts;
+// after page loads activate function
+window.onload = hideAndShow;
