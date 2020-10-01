c30fa6ca532909f9e6cf5e0daf050220e30ba956
gnarlee-gee <charlesgrealy@gmail.com>
2020-10-01 18:12:00 -0500

    show posts upon clicking drop down folder

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -1,28 +1,44 @@
-// Grab the info div and change it's z-index to allow scrolling above nav divs
-function hideAndShow() {
-    var checkedItem = document.getElementById('hamburger');
-    var changeZindex = document.getElementById('info-box');
-    var hideName = document.getElementById('home-link');
-    var hideNavBar = document.getElementById('nav-bar');
-    var checkLabel = document.getElementById('check-label')
-    changeZindex.setAttribute('style', 'z-index: 1;');
-    checkedItem.addEventListener('click', event => {
-        if (checkedItem.checked) {
-            changeZindex.setAttribute('style', 'z-index: 0;');
-            changeZindex.setAttribute('style', 'opacity: 0;');
-            hideName.setAttribute('style', 'opacity: 0;');
-            hideNavBar.setAttribute('style', 'background: rgba(32, 32, 32, 1)');
-            checkLabel.setAttribute('style', 'border: 4px solid transparent;');
+// Change right arrow to a down arrow on click and vice versa
+function dropDownBlog() {
+    const blogBox = document.querySelector('.blog-box');
+    const rightArrow = document.querySelector("#right-arrow");
+    const downArrow = document.querySelector("#down-arrow");
+    const posts = document.querySelector('.blog-posts')
+    blogBox.addEventListener('click', event => {
+        if (rightArrow.style.display == 'inline-block'){
+            rightArrow.style.display = 'none';
+            downArrow.style.display = 'inline-block';
+            posts.style.display = 'block';
         } else {
-            changeZindex.setAttribute('style', 'z-index: 1;');
-            changeZindex.setAttribute('style', 'opacity: 1;');
-            hideName.setAttribute('style', 'opacity: 1;');
-            hideNavBar.setAttribute('style', 'opacity: 1;');
-            hideNavBar.removeAttribute('style', 'background: rgba(32, 32, 32, 1)');
-            checkLabel.removeAttribute('style', 'border: 4px solid transparent;');
+            downArrow.style.display = 'none';
+            rightArrow.style.display = 'inline-block';
+            posts.style.display = 'none';
         }
+
     })
+    // var checkedItem = document.getElementById('hamburger');
+    // var changeZindex = document.getElementById('info-box');
+    // var hideName = document.getElementById('home-link');
+    // var hideNavBar = document.getElementById('nav-bar');
+    // var checkLabel = document.getElementById('check-label')
+    // changeZindex.setAttribute('style', 'z-index: 1;');
+    // checkedItem.addEventListener('click', event => {
+    //     if (checkedItem.checked) {
+    //         changeZindex.setAttribute('style', 'z-index: 0;');
+    //         changeZindex.setAttribute('style', 'opacity: 0;');
+    //         hideName.setAttribute('style', 'opacity: 0;');
+    //         hideNavBar.setAttribute('style', 'background: rgba(32, 32, 32, 1)');
+    //         checkLabel.setAttribute('style', 'border: 4px solid transparent;');
+    //     } else {
+    //         changeZindex.setAttribute('style', 'z-index: 1;');
+    //         changeZindex.setAttribute('style', 'opacity: 1;');
+    //         hideName.setAttribute('style', 'opacity: 1;');
+    //         hideNavBar.setAttribute('style', 'opacity: 1;');
+    //         hideNavBar.removeAttribute('style', 'background: rgba(32, 32, 32, 1)');
+    //         checkLabel.removeAttribute('style', 'border: 4px solid transparent;');
+    //     }
+    // })
 }
 
 // after page loads activate function
-window.onload = hideAndShow;
+window.onload = dropDownBlog;
