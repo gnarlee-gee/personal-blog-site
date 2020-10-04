1c5835dc8bb11efc7425c0de1f3eb9f034774667
gnarlee-gee <charlesgrealy@gmail.com>
2020-10-03 21:21:55 -0500

    fine tune the side menu and initialize grid to acquaint menu items

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -1,20 +1,34 @@
-const blogPosts = document.querySelector('.blog-posts');
-const numOfPosts = blogPosts.children.length;
+// after page loads activate function
+window.onload = function () {
+    const blogPosts = document.querySelector('.blog-posts');
+    const numOfPosts = blogPosts.children.length;
+    blogClick(numOfPosts);
+    initializeNavGrid(numOfPosts);
+}
 
-// Moves contact.html and icon down according to number of posts
+
+// Moves ".html" items and their icons down according to number of posts
 // when the drop down folder is clicked
-function changeContactRows(operation) {
-    let resumeRowStartNum = (numOfPosts + 3).toString();
+function changeContactRows(operation, numOfPosts) {
+    let projectsRowStartNum = (numOfPosts + 3).toString();
+    let projectsRowEndNum = projectsRowStartNum.toString(projectsRowStartNum++);
+    let resumeRowStartNum = (numOfPosts + 4).toString();
     let resumeRowEndNum = resumeRowStartNum.toString(resumeRowStartNum++);
-    let contactRowStartNum = (numOfPosts + 4).toString();
+    let contactRowStartNum = (numOfPosts + 5).toString();
     let contactRowEndNum = contactRowStartNum.toString(contactRowStartNum++);
 
-    const resumeIcon = document.querySelector('.html-icon-2');
+    const projectsIcon = document.querySelector('.html-icon-2');
+    const projects = document.querySelector('.projects-side');
+    const resumeIcon = document.querySelector('.html-icon-3');
     const resume = document.querySelector('.resume-side');
-    const contactIcon = document.querySelector('.html-icon-3');
+    const contactIcon = document.querySelector('.html-icon-4');
     const contact = document.querySelector('.contact-side');
 
-    if (operation == 'change'){
+    if (operation == 'change') {
+        projectsIcon.style.gridRowStart = projectsRowStartNum;
+        projectsIcon.style.gridRowEnd = projectsRowEndNum;
+        projects.style.gridRowStart = projectsRowStartNum;
+        projects.style.gridRowEnd = projectsRowEndNum;
         resumeIcon.style.gridRowStart = resumeRowStartNum;
         resumeIcon.style.gridRowEnd = resumeRowEndNum;
         resume.style.gridRowStart = resumeRowStartNum;
@@ -24,74 +38,51 @@ function changeContactRows(operation) {
         contact.style.gridRowStart = contactRowStartNum;
         contact.style.gridRowEnd = contactRowEndNum;
     } else {
-        resumeIcon.style.gridRowStart = 3;
-        resumeIcon.style.gridRowEnd = 4;
-        resume.style.gridRowStart = 3;
-        resume.style.gridRowEnd = 4;
-        contactIcon.style.gridRowStart = 4;
-        contactIcon.style.gridRowEnd = 5;
-        contact.style.gridRowStart = 4;
-        contact.style.gridRowEnd = 5;
-
+        projectsIcon.style.gridRowStart = 3;
+        projectsIcon.style.gridRowEnd = 4;
+        projects.style.gridRowStart = 3;
+        projects.style.gridRowEnd = 4;
+        resumeIcon.style.gridRowStart = 4;
+        resumeIcon.style.gridRowEnd = 5;
+        resume.style.gridRowStart = 4;
+        resume.style.gridRowEnd = 5;
+        contactIcon.style.gridRowStart = 5;
+        contactIcon.style.gridRowEnd = 6;
+        contact.style.gridRowStart = 5;
+        contact.style.gridRowEnd = 6;
     }
-
 }
 
 // Change right arrow to a down arrow on click and vice versa
-function dropDownBlog() {
+// Modifies behavior when clicking blog
+function blogClick(numOfPosts) {
     const blogBox = document.querySelector('.blog-box');
     const rightArrow = document.querySelector("#right-arrow");
     const downArrow = document.querySelector("#down-arrow");
     const posts = document.querySelector('.blog-posts')
-    
-    if (rightArrow.style.display == 'inline-block'){
+
+    if (rightArrow.style.display == 'inline-block') {
         posts.style.display = 'none';
     }
-
     blogBox.addEventListener('click', event => {
-        if (rightArrow.style.display == 'inline-block'){
+        if (rightArrow.style.display == 'inline-block') {
             rightArrow.style.display = 'none';
             downArrow.style.display = 'inline-block';
             posts.style.display = 'block';
-            changeContactRows('change');
+            changeContactRows('change', numOfPosts);
         } else {
             downArrow.style.display = 'none';
             rightArrow.style.display = 'inline-block';
             posts.style.display = 'none';
-            changeContactRows();
-            // contactIcon.style.gridRowStart = '3';
-            // contactIcon.style.gridRowEnd = '4';
+            changeContactRows('default', numOfPosts);
         }
-
     })
-    // var checkedItem = document.getElementById('hamburger');
-    // var changeZindex = document.getElementById('info-box');
-    // var hideName = document.getElementById('home-link');
-    // var hideNavBar = document.getElementById('nav-bar');
-    // var checkLabel = document.getElementById('check-label')
-    // changeZindex.setAttribute('style', 'z-index: 1;');
-    // checkedItem.addEventListener('click', event => {
-    //     if (checkedItem.checked) {
-    //         changeZindex.setAttribute('style', 'z-index: 0;');
-    //         changeZindex.setAttribute('style', 'opacity: 0;');
-    //         hideName.setAttribute('style', 'opacity: 0;');
-    //         hideNavBar.setAttribute('style', 'background: rgba(32, 32, 32, 1)');
-    //         checkLabel.setAttribute('style', 'border: 4px solid transparent;');
-    //     } else {
-    //         changeZindex.setAttribute('style', 'z-index: 1;');
-    //         changeZindex.setAttribute('style', 'opacity: 1;');
-    //         hideName.setAttribute('style', 'opacity: 1;');
-    //         hideNavBar.setAttribute('style', 'opacity: 1;');
-    //         hideNavBar.removeAttribute('style', 'background: rgba(32, 32, 32, 1)');
-    //         checkLabel.removeAttribute('style', 'border: 4px solid transparent;');
-    //     }
-    // })
 }
 
+// Set # of grid items based on number of posts + 4 static items (home, projects, etc...)
+function initializeNavGrid(numOfPosts) {
+    let navBar = document.querySelector('.nav');
+    const totalMenuItems = (numOfPosts + 4).toString();
 
-
-// after page loads activate function
-window.onload = function () {
-    dropDownBlog();
-    // getCountOfP();
+    navBar.style.setProperty('grid-template-columns', `repeat(${totalMenuItems}, 1fr)`);
 }
