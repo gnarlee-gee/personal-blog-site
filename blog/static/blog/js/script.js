89dc964911ecdb1da2b00aba4bd04d379804af30
gnarlee-gee <charlesgrealy@gmail.com>
2020-11-10 21:38:13 -0600

    delete one sidepane grid and combine into a single grid, adjust size of sidepane and info-box, redesign grid, delete nav bar, adjust resize jquery

--- blog/static/blog/js/script.js
+++ blog/static/blog/js/script.js
@@ -3,14 +3,14 @@ window.onload = function () {
     const blogPosts = document.querySelector('.blog-posts');
     let numOfPosts = blogPosts.children.length;
     numOfPosts /= 2;
-    setIdBlogPosts(blogPosts, numOfPosts);
-    initializeSidePane();
+    setIdBlogPosts(blogPosts);
+    // initializeSidePane();
     blogClick(numOfPosts);
-    initializeNavGrid(numOfPosts);
-    addSidepaneItemsListener();
+    // initializeNavGrid(numOfPosts);
+    // addSidepaneItemsListener();
     resizeSidepane();
-    isInNav();
-    removeFromNavBar();
+    // isInNav();
+    // removeFromNavBar();
 
 
 
@@ -30,7 +30,7 @@ function setIdBlogPosts(blogPosts, numOfPosts) {
 
 // Moves ".html" items and their icons down according to number of posts
 // when the drop down folder is clicked
-function changeIconTextRows(operation, numOfPosts) {
+function moveGridItems(operation, numOfPosts) {
     let projectsRowStartNum = (numOfPosts + 3).toString();
     let projectsRowEndNum = projectsRowStartNum.toString(projectsRowStartNum++);
     let resumeRowStartNum = (numOfPosts + 4).toString();
@@ -38,47 +38,32 @@ function changeIconTextRows(operation, numOfPosts) {
     let contactRowStartNum = (numOfPosts + 5).toString();
     let contactRowEndNum = contactRowStartNum.toString(contactRowStartNum++);
 
-    const projectsIcon = document.querySelector('.html-icon-2');
     const projects = document.querySelector('.projects-side');
-    const resumeIcon = document.querySelector('.html-icon-3');
     const resume = document.querySelector('.resume-side');
-    const contactIcon = document.querySelector('.html-icon-4');
     const contact = document.querySelector('.contact-side');
 
     if (operation == 'change') {
-        projectsIcon.style.gridRowStart = projectsRowStartNum;
-        projectsIcon.style.gridRowEnd = projectsRowEndNum;
         projects.style.gridRowStart = projectsRowStartNum;
         projects.style.gridRowEnd = projectsRowEndNum;
-        resumeIcon.style.gridRowStart = resumeRowStartNum;
-        resumeIcon.style.gridRowEnd = resumeRowEndNum;
         resume.style.gridRowStart = resumeRowStartNum;
         resume.style.gridRowEnd = resumeRowEndNum;
-        contactIcon.style.gridRowStart = contactRowStartNum;
-        contactIcon.style.gridRowEnd = contactRowEndNum;
         contact.style.gridRowStart = contactRowStartNum;
         contact.style.gridRowEnd = contactRowEndNum;
     } else {
-        projectsIcon.style.gridRowStart = 3;
-        projectsIcon.style.gridRowEnd = 4;
         projects.style.gridRowStart = 3;
         projects.style.gridRowEnd = 4;
-        resumeIcon.style.gridRowStart = 4;
-        resumeIcon.style.gridRowEnd = 5;
         resume.style.gridRowStart = 4;
         resume.style.gridRowEnd = 5;
-        contactIcon.style.gridRowStart = 5;
-        contactIcon.style.gridRowEnd = 6;
         contact.style.gridRowStart = 5;
         contact.style.gridRowEnd = 6;
     }
 }
 
 // Adds and removes grid items on click
-function setBlogPostsGrid(operation, numOfPosts) {
+function setBlogPostsGrid(operation) {
     let parent = document.querySelector('.blog-posts');
     let children = parent.children;
-
+    let posts = document.querySelectorAll('.posts')
     children = Math.floor(children.length / 2);
 
     if (operation == 'change') {
@@ -86,30 +71,33 @@ function setBlogPostsGrid(operation, numOfPosts) {
         for (; i < children; i++) {
             let postBox = document.createElement('div');
             postBox.setAttribute('id', `post-box-${i+1}`);
+            // postBox.setAttribute('class', 'posts')
             document.getElementById("sidepane-div").appendChild(postBox);
             postBox = document.querySelector(`#post-box-${i+1}`);
+            postBox.innerHTML = "<p>" + posts[i].innerHTML + "</p>"
+            postBox.setAttribute('class', 'posts')
             // Send the id of these to put click event on
             postBox.style.zIndex = 2;
             postBox.style.gridRowStart = i + 3;
             postBox.style.gridRowEnd = i + 4;
         }
-        document.querySelector('#projects-box').style.gridRowStart = i + 3;
-        document.querySelector('#projects-box').style.gridRowEnd = i + 4;
-        document.querySelector('#resume-box').style.gridRowStart = i + 4;
-        document.querySelector('#resume-box').style.gridRowEnd = i + 5;
-        document.querySelector('#contact-box').style.gridRowStart = i + 5;
-        document.querySelector('#contact-box').style.gridRowEnd = i + 6;
+        document.querySelector('.projects-side').style.gridRowStart = i + 3;
+        document.querySelector('.projects-side').style.gridRowEnd = i + 4;
+        document.querySelector('.resume-side').style.gridRowStart = i + 4;
+        document.querySelector('.resume-side').style.gridRowEnd = i + 5;
+        document.querySelector('.contact-side').style.gridRowStart = i + 5;
+        document.querySelector('.contact-side').style.gridRowEnd = i + 6;
     } else if (operation == 'default') {
         for (let i = 0; i < children; i++) {
             let post = document.querySelector(`#post-box-${i+1}`);
             post.remove();
         }
-        document.querySelector('#projects-box').style.gridRowStart = 3;
-        document.querySelector('#projects-box').style.gridRowEnd = 4;
-        document.querySelector('#resume-box').style.gridRowStart = 4;
-        document.querySelector('#resume-box').style.gridRowEnd = 5;
-        document.querySelector('#contact-box').style.gridRowStart = 5;
-        document.querySelector('#contact-box').style.gridRowEnd = 6;
+        document.querySelector('.projects-side').style.gridRowStart = 3;
+        document.querySelector('.projects-side').style.gridRowEnd = 4;
+        document.querySelector('.resume-side').style.gridRowStart = 4;
+        document.querySelector('.resume-side').style.gridRowEnd = 5;
+        document.querySelector('.contact-side').style.gridRowStart = 5;
+        document.querySelector('.contact-side').style.gridRowEnd = 6;
     }
 }
 
@@ -117,154 +105,160 @@ function setBlogPostsGrid(operation, numOfPosts) {
 // Change right arrow to a down arrow on click and vice versa
 // Modifies behavior when clicking blog
 function blogClick(numOfPosts) {
-    const blogBox = document.querySelector('#blog-box');
-    const rightArrow = document.querySelector("#right-arrow");
+    const blogBox = document.querySelector('.blog-side');
     const downArrow = document.querySelector("#down-arrow");
+    const upArrow = document.querySelector("#up-arrow");
     const posts = document.querySelector('.blog-posts')
 
-    if (rightArrow.style.display == 'inline-block') {
+    if (downArrow.style.display == 'inline-block') {
         posts.style.display = 'none';
     }
     blogBox.addEventListener('click', event => {
-        if (rightArrow.style.display == 'inline-block') {
-            rightArrow.style.display = 'none';
-            downArrow.style.display = 'inline-block';
+        if (downArrow.style.display == 'inline-block') {
+            console.log('works')
+            downArrow.style.display = 'none';
+            upArrow.style.display = 'inline-block';
             posts.style.display = 'block';
-            changeIconTextRows('change', numOfPosts);
+            moveGridItems('change', numOfPosts);
             setBlogPostsGrid('change', numOfPosts)
             // console.log(document.querySelector("#sidepane-div").children)
 
         } else {
-            downArrow.style.display = 'none';
-            rightArrow.style.display = 'inline-block';
+            upArrow.style.display = 'none';
+            downArrow.style.display = 'inline-block';
             posts.style.display = 'none';
-            changeIconTextRows('default', numOfPosts);
+            moveGridItems('default', numOfPosts);
             setBlogPostsGrid('default', numOfPosts);
         }
     })
 }
 
 // Set # of grid items based on number of posts + 4 static items (home, projects, etc...)
-function initializeNavGrid(numOfPosts) {
-    let navBar = document.querySelector('.nav');
-    const totalMenuItems = (numOfPosts + 4).toString();
+// function initializeNavGrid(numOfPosts) {
+//     let navBar = document.querySelector('.nav');
+//     const totalMenuItems = (numOfPosts + 4).toString();
 
-    navBar.style.setProperty('grid-template-columns', `repeat(${totalMenuItems}, 1fr)`);
-}
+//     navBar.style.setProperty('grid-template-columns', `repeat(${totalMenuItems}, 1fr)`);
+// }
 
 // Implements ability to resize sidepane
 function resizeSidepane() {
     $(function () {
         $(".sidepane").resizable({
             handles: "e",
-            minWidth: 200,
-            alsoResize: ".blog-posts"
+            minWidth: 225,
+            alsoResize: ".posts"
         })
     });
     $(function () {
-        $(".blog-posts").resizable({
+        $(".posts").resizable({
             handles: "e",
-            minWidth: 132,
+            minWidth: 225,
         })
     });
 }
 
-function initializeSidePane() {
+// function initializeSidePane() {
 
-    let homeBox = document.createElement('div');
-    let blogBox = document.createElement('div');
-    let projectsBox = document.createElement('div');
-    let resumeBox = document.createElement('div');
-    let contactBox = document.createElement('div');
-
-
-    homeBox.setAttribute('id', 'home-box');
-    blogBox.setAttribute('id', 'blog-box');
-    projectsBox.setAttribute('id', 'projects-box');
-    resumeBox.setAttribute('id', 'resume-box');
-    contactBox.setAttribute('id', 'contact-box');
-
-    document.getElementById("sidepane-div").appendChild(homeBox);
-    document.getElementById("sidepane-div").appendChild(blogBox);
-    document.getElementById("sidepane-div").appendChild(projectsBox);
-    document.getElementById("sidepane-div").appendChild(resumeBox);
-    document.getElementById("sidepane-div").appendChild(contactBox);
+//     let homeBox = document.createElement('div');
+//     let blogBox = document.createElement('div');
+//     let projectsBox = document.createElement('div');
+//     let resumeBox = document.createElement('div');
+//     let contactBox = document.createElement('div');
 
 
-    // console.log(document.querySelector("#sidepane-div").children)
-    homeBox = document.querySelector('#home-box');
-    homeBox.style.zIndex = 2;
-    homeBox.style.gridRowStart = 1;
-    homeBox.style.gridRowEnd = 2;
+//     homeBox.setAttribute('id', 'home-box');
+//     blogBox.setAttribute('id', 'blog-box');
+//     projectsBox.setAttribute('id', 'projects-box');
+//     resumeBox.setAttribute('id', 'resume-box');
+//     contactBox.setAttribute('id', 'contact-box');
 
-    blogBox = document.querySelector('#blog-box');
-    blogBox.style.zIndex = 2;
-    blogBox.style.gridRowStart = 2;
-    blogBox.style.gridRowEnd = 3;
-
-    projectsBox = document.querySelector('#projects-box');
-    projectsBox.style.zIndex = 2;
-    projectsBox.style.gridRowStart = 3;
-    projectsBox.style.gridRowEnd = 4;
-
-    resumeBox = document.querySelector('#resume-box');
-    resumeBox.style.zIndex = 2;
-    resumeBox.style.gridRowStart = 4;
-    resumeBox.style.gridRowEnd = 5;
-
-    contactBox = document.querySelector('#contact-box');
-    contactBox.style.zIndex = 2;
-    contactBox.style.gridRowStart = 5;
-    contactBox.style.gridRowEnd = 6;
-}
+//     document.getElementById("sidepane-div").appendChild(homeBox);
+//     document.getElementById("sidepane-div").appendChild(blogBox);
+//     document.getElementById("sidepane-div").appendChild(projectsBox);
+//     document.getElementById("sidepane-div").appendChild(resumeBox);
+//     document.getElementById("sidepane-div").appendChild(contactBox);
 
 
-function addSidepaneItemsListener() {
-    let sidepaneChildren = document.querySelector("#sidepane-div").children;
-    for (let i = 0; i < sidepaneChildren.length; i++) {
-        let child = document.querySelector('#' + sidepaneChildren[i].id);
-        child.addEventListener('click', event => {
-            // console.log(child);
-            addToNavBar(child);
-        })
+//     // console.log(document.querySelector("#sidepane-div").children)
+//     homeBox = document.querySelector('#home-box');
+//     homeBox.style.zIndex = 2;
+//     homeBox.style.gridRowStart = 1;
+//     homeBox.style.gridRowEnd = 2;
 
-    }
-}
+//     blogBox = document.querySelector('#blog-box');
+//     blogBox.style.zIndex = 2;
+//     blogBox.style.gridRowStart = 2;
+//     blogBox.style.gridRowEnd = 3;
 
+//     projectsBox = document.querySelector('#projects-box');
+//     projectsBox.style.zIndex = 2;
+//     projectsBox.style.gridRowStart = 3;
+//     projectsBox.style.gridRowEnd = 4;
 
-function addToNavBar(child) {
-    let navBar = document.querySelector('.nav');
-    let navStyle = getComputedStyle(navBar).gridTemplateAreas;
-    navStyle = navStyle.replace(/"/g, '')
-    const childIdNavName = child.id.split('-')
+//     resumeBox = document.querySelector('#resume-box');
+//     resumeBox.style.zIndex = 2;
+//     resumeBox.style.gridRowStart = 4;
+//     resumeBox.style.gridRowEnd = 5;
 
-    if (childIdNavName[0] != 'post') {
-        if (!navStyle.includes(childIdNavName[0])) {
-            navBar.style.gridTemplateAreas = `"${navStyle + ' ' + childIdNavName[0]}"`;
-            console.log('navStyle', getComputedStyle(navBar).gridTemplateAreas)
-        }
-    } else {
-        if (!navStyle.includes(childIdNavName.join('-'))) {
-            navBar.style.gridTemplateAreas = `"${navStyle + ' ' + childIdNavName.join('-')}"`
-            console.log('navStyle', getComputedStyle(navBar).gridTemplateAreas)
-        }
-    }
-}
-
-function setNavClassStyle(navItem, className) {
-    navItem.style.display = 'flex';
-    navItem.style.alignItems = 'center';
-    navItem.style.justifyContent = 'center';
-    navItem.style.gridArea = className;
-    navItem.style.padding = "0 10px 0 10px";
-
-}
+//     contactBox = document.querySelector('#contact-box');
+//     contactBox.style.zIndex = 2;
+//     contactBox.style.gridRowStart = 5;
+//     contactBox.style.gridRowEnd = 6;
+// }
 
 
-function isInNav() {
+// function addSidepaneItemsListener() {
+//     let sidepaneChildren = document.querySelector("#sidepane-div").children;
+//     console.log(sidepaneChildren[1])
+//     for (let i = 0; i < sidepaneChildren.length; i++) {
+//         // let child = document.querySelector('#' + sidepaneChildren[i].id);
+//         sidepaneChildren[i].addEventListener('click', event => {
+//             // console.log(child);
+//             addToNavBar(sidepaneChildren[i]);
+//         })
 
-}
+//     }
+// }
 
-function removeFromNavBar() {
 
-}
+// function addToNavBar(child) {
+//     let navBar = document.querySelector('.nav');
+//     let navStyle = getComputedStyle(navBar).gridTemplateAreas;
+//     navStyle = navStyle.replace(/"/g, '')
+//     const childIdNavName = child.id.split('-')
+
+//     if (childIdNavName[0] != 'post') {
+//         if (!navStyle.includes(childIdNavName[0])) {
+//             navBar.style.gridTemplateAreas = `"${navStyle + ' ' + childIdNavName[0]}"`;
+//             console.log('navStyle', getComputedStyle(navBar).gridTemplateAreas)
+//         }
+//     } else {
+//         if (!navStyle.includes(childIdNavName.join('-'))) {
+//             navBar.style.gridTemplateAreas = `"${navStyle + ' ' + childIdNavName.join('-')}"`
+//             console.log('navStyle', getComputedStyle(navBar).gridTemplateAreas)
+//         }
+//     }
+// }
+
+// function setNavClassStyle(navItem, className) {
+
+//     navItem.setAttribute('style', 
+//     `display: center;
+//     align-items: center;
+//     justify-content: center;
+//     grid-area: ${className};
+//     padding: 0 10px 0 10px;`);
+//     // navItem.style.display = 'flex';
+//     // navItem.style.alignItems = 'center';
+//     // navItem.style.justifyContent = 'center';
+//     // navItem.style.gridArea = className;
+//     // navItem.style.padding = "0 10px 0 10px";
+
+// }
+
+
+
+// function removeFromNavBar() {
+
+// }
