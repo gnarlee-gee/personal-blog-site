8470d83345d98083f9f9cf88ace87939fbd1ff7f
gnarlee-gee <charlesgrealy@gmail.com>
2020-11-28 18:09:13 -0600

    deploy to heroku

--- blog/models.py
+++ blog/models.py
@@ -9,13 +9,13 @@ import datetime
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=255, null=True)
+    title = models.CharField(max_length=255)
     
     # on_delete will delete all posts by admin IF we delete that admin
     body = models.TextField(null=True)
     
-    date = models.DateField(auto_now_add=False, null=True)
-    slug = models.SlugField(editable=False, unique=True, null=True)
+    date = models.DateField(auto_now_add=False)
+    slug = models.SlugField(default='',editable=False, unique=True)
     
     class Meta:
         ordering = ['-pk']
