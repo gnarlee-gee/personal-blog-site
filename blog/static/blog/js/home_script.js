17100bc8a4b641b441a61c6a22f47aa8ff3f3f4d
gnarlee-gee <charlesgrealy@gmail.com>
2020-12-21 11:57:40 -0600

    add divs to help IO work efficiently for fast scroll case

--- blog/static/blog/js/home_script.js
+++ blog/static/blog/js/home_script.js
@@ -16,34 +16,38 @@ const projectSection = document.querySelector(".projects-section");
 const resumeMenuText = document.querySelector("#resume-side-text");
 const resumeSection = document.querySelector(".resume-section");
 
+const ioDivs = document.querySelectorAll(".observer-box");
+
+
 const menuText = [homeMenuText, blogMenuText, projectsMenuText, resumeMenuText];
 const menuSection = [homeSection, blogSection, projectSection, resumeSection];
 
-
 function changeMenuItemColor(menuItem) {
     switch (menuItem) {
-        case 'about-me':
+        case 'home':
             homeMenuText.style.color = "#090B0D";
             homeMenuText.classList.remove("menu-hover");
             blogMenuText.style.color = "#D7D9D9";
             projectsMenuText.style.color = "#D7D9D9";
             resumeMenuText.style.color = "#D7D9D9";
             break;
-        case 'blog-section':
+        case 'blog':
             homeMenuText.style.color = "#D7D9D9";
             blogMenuText.style.color = "#090B0D";
             blogMenuText.classList.remove("menu-hover");
             projectsMenuText.style.color = "#D7D9D9";
             resumeMenuText.style.color = "#D7D9D9";
             break;
-        case 'projects-section':
-            projectsMenuText.style.color = "#090B0D";
+        case 'project':
+            if (projectsMenuText.style.color != "#090B0D") {
+                projectsMenuText.style.color = "#090B0D";
+            }
             projectsMenuText.classList.remove("menu-hover");
             resumeMenuText.style.color = "#D7D9D9";
             blogMenuText.style.color = "#D7D9D9";
             homeMenuText.style.color = "#D7D9D9";
             break;
-        case 'resume-section':
+        case 'resume':
             resumeMenuText.style.color = "#090B0D";
             resumeMenuText.classList.remove("menu-hover");
             projectsMenuText.style.color = "#D7D9D9";
@@ -57,18 +61,25 @@ function changeMenuItemColor(menuItem) {
 let observer = new IntersectionObserver(function (entries) {
     // isIntersecting is true when element and viewport are overlapping
     // isIntersecting is false when element and viewport don't overlapc
-
-    if (entries[0].isIntersecting) {
-        changeMenuItemColor(entries[0].target.className);
-        addHoverMenu(entries[0].target.id);
-    }
-
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            let firstClass = entry.target.className;
+            // console.log(str.substr(0,str.indexOf(' ')));
+            changeMenuItemColor(firstClass.substr(0, firstClass.indexOf(' ')));
+            addHoverMenu(firstClass.substr(0, firstClass.indexOf(' ')));
+        }
+    })
+    // if (entries[0].isIntersecting) {
+    //     console.log(entries[0].target.className)
+    //     // changeMenuItemColor(entries[0].target.className);
+    //     // addHoverMenu(entries[0].target.id);
+    // }
 }, {
-    rootMargin: '0px 0px -95% 0px',
+    rootMargin: '0px 0px -50% 0px',
 });
 
 function connect() {
-    menuSection.forEach((item) => {
+    ioDivs.forEach((item) => {
         observer.observe(item);
     })
 }
@@ -77,8 +88,10 @@ function scrollTo() {
 
     menuText.forEach((item, index) => {
         item.addEventListener('click', () => {
-            menuSection.forEach((menuItem) => {
-                if (!item.id.includes(menuItem.id)) {
+            ioDivs.forEach((menuItem) => {
+                let menuItemText = menuItem.className;
+                menuItemText = menuItemText.substr(0, menuItemText.indexOf(' '));
+                if (!item.id.includes(menuItemText)) {
                     observer.unobserve(menuItem);
                 }
             });
@@ -87,9 +100,11 @@ function scrollTo() {
                 behavior: 'smooth',
             });
 
-            menuSection.forEach((menuItem) => {
-                if (!item.id.includes(menuItem.id)) {
-                    setTimeout(function(){
+            ioDivs.forEach((menuItem) => {
+                let menuItemText = menuItem.className;
+                menuItemText = menuItemText.substr(0, menuItemText.indexOf(' '));
+                if (!item.id.includes(menuItemText)) {
+                    setTimeout(function () {
                         observer.observe(menuItem);
                     }, 500);
                 }
@@ -105,4 +120,3 @@ function addHoverMenu(element) {
         }
     });
 }
-
